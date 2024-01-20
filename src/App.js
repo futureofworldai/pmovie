2515f72e223c9881793c20f2005eaaa523019916
student <dhirajchavan355@gmail.com>
2024-01-20 19:29:44 +0530

    added api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from "react";
+import "./App.css";
+import SearchIcon from "./search.svg"
+const API_URL='http://www.omdbapi.com?apikey=d0db8a45';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const post={
+    "Title": "Batman: The Animated Series",
+    "Year": "1992–1995",
+    "imdbID": "tt0103359",
+    "Type": "series",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOTM3MTRkZjQtYjBkMy00YWE1LTkxOTQtNDQyNGY0YjYzNzAzXkEyXkFqcGdeQXVyOTgwMzk1MTA@._V1_SX300.jpg"
+}
+
+const App = () => {
+    const searchMovies=async(title)=>{
+        const response=await fetch(`${API_URL}&s=${title}`);
+        const data=await response.json();
+
+        console.log(data.Search);
+    }
+    useEffect(()=>{
+        searchMovies('batman');
+    
+    },[]);
+
+ return(
+       <>
+       <div className="app">
+            <h1>MoviesWorld</h1>
+            
+
+        <div className="search">
+           <input
+            placeholder="Search for a movie"
+            value="batman"
+            onChange={()=>{}}
+            />
+            <img
+            src={SearchIcon}
+            alt="Search Icon"
+            onClick={()=>{}}
+            />
+            </div>
+
+            <div className="container">
+                 <div className="movie">
+
+                    <div>
+                        <p>{post.Year}</p>
+                    </div>
+
+                    <div>
+                        <img src={post.Poster} alt={post.Title}/>
+                    </div>
+                    </div>
+
+                </div>
+        </div>
+        </>
+    );
+ 
 }
 
 export default App;
